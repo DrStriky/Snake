bd1b4d378160eda857b5f17097562200738a5256
Florian Pribahsnik <Florian.Pribahsnik@gmx.at>
2020-08-17 15:09:09 +0200

    implemented all game features

--- game.py
+++ game.py
@@ -1,5 +1,6 @@
 from typing import Tuple, Type
 import numpy as np
+import random
 
 from snake import Snake
 from board import Board
@@ -21,19 +22,22 @@ class Game:
     :ivar food: instance of the food
     """
 
-    def __init__(self, board_dim: np.ndarray, snake_start: Tuple[int, int], food_start: Tuple[int, int]):
-        self.game_store = 0
-        self.board = Board(board_dim)
-        self.snake = Snake(snake_start)
+    def __init__(self, board_dim: np.ndarray, cube_size: int, snake_start: Tuple[int, int], food_start: Tuple[int, int], random_seed: int):
+        self.game_score = 0
+        self.board = Board(board_dim, cube_size)
+        self.snake = Snake([snake_start])
         self.food = Apple(food_start)
 
-    def run_game(self, Interaction_Handler: Type[Interaction_Handler]) -> None:
+        self.random = random.Random(random_seed)
+
+    def run_game(self, interaction_handler: Type[Interaction_Handler]) -> None:
         """
         Updates the current score using the discount function.
 
         :return:
         """
-        inter_handler= Interaction_Handler()
+        alive = True
+        inter_handler = interaction_handler()
 
         while alive:
             eaten = False
@@ -54,20 +58,16 @@ class Game:
             if self.snake.update(eaten):
                 alive = False
                 print('Your snake touched itself!')
-            self.food.update()
-            self.self.game_store += self.food.score
+            if eaten:
+                self.game_score += self.food.score
+                self.food = Apple((self.random.randint(1, self.board.board_matrix.shape[0]), self.random.randint(1, self.board.board_matrix.shape[1])))  # TODO random position for new food
+            else:
+                self.food.update()
 
-            snake update
-            apple update
-            score update
+            # TODO write new Besetzungsmatrix
 
-            write new besetzungsmatrix
-
-            next tick
-
-    def get_occupying_matrix():
-        print('blavlalv')
-
-
-
-
+    def get_occupying_matrix(self):
+        self.board.board_matrix
+        self.snake.body
+        self.food.position
+        return self
