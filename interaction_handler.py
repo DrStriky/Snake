a02c9b7fefee14ab1f54c61400ae9c53e132b9b1
Jonathan Gantner <joanthan.gantner@gmx.at>
2020-08-17 12:28:43 +0200

    Added InteractionHandler Interface

---
+++ interaction_handler.py
@@ -0,0 +1,33 @@
+import numpy as np
+from typing import TypedDict, Literal
+
+class BoardEncodingDict(TypedDict):
+    """
+    TypedDict class for enconding of different elements in board
+    """
+    head: int
+    wall: int
+    valid: int
+    snake: int
+    food: int
+
+
+direction = Literal[(0, 1), (1, 0), (0, -1), (-1, 0)]
+
+
+class InteractionHandler:
+    """
+    Informal interface for handling GUI and user interactions.
+    """
+    def get_encoding_dict(self) -> BoardEncodingDict:
+        """Returns the encoding for different board elements."""
+        pass
+
+    def draw_board(self, geometry_matrix: np.array) -> None:
+        """Updates the GUI for the next tick."""
+        pass
+
+    def get_interaction(self) -> direction:
+        """Waits for player's input and returns the moving direction for the next snake"""
+        pass
+
