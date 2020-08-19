2dc365a398d14760eb748801ef2b5526e5e1d4ab
Florian Pribahsnik <Florian.Pribahsnik@gmx.at>
2020-08-19 14:46:24 +0200

    integrated player

--- main_with_AI.py
+++ main_with_AI.py
@@ -5,6 +5,7 @@ from pygame_interactions import PygameInteractions
 import pygame
 from pytorch_interactions import PyGamePyTorchInteractionHandler
 from pytorch_player import PyTorchPlayer
+import random
 
 
 def edge_mask(x: np.ndarray):
@@ -31,11 +32,34 @@ def main():
 
     our_player = PyTorchPlayer()
     interacter = PyGamePyTorchInteractionHandler(player=our_player, display=display, block_length=block_size,
-                                                 ticks_per_second=5)
+                                                 ticks_per_second=10)
+    our_player.encoding = interacter.get_encoding_dict()
 
-    game = Game(dummy, 25, (3, 3), (3, 10), 42)
+    total_score = 0
+    while True:
+        game = Game(dummy, 25, (3, 3), (random.randint(1,9), random.randint(1,9)), 42)
+        our_player.counter_move = 0
+        game.run_game(interacter)
 
-    game.run_game(interacter)
+        # One game is over, train on the memory and plot the result.
+        total_score += game.game_score
+        our_player.train_long_memory(our_player.memory)
+        print('Game', our_player.counter_games, '      Score:', game.game_score)
+
+
+    if sc > record:
+        record = sc
+        name = 'best_model.pth'.format(sc)
+        dir = os.path.join(model_folder_path, name)
+        torch.save(our_player.model.state_dict(), dir)
+    print('record: ', record)
+    score_plot.append(sc)
+    mean = total_score / agent.counter_games
+    mean_plot.append(mean)
+    agent.plot(score_plot, mean_plot)
+
+    plt.ioff()
+    plt.show()
 
 
 if __name__ == "__main__":
